17b8f6baf28e8afa00a43eb998c9f33b91379981
Bruno Beghelli <bruno.beghelli@gmail.com>
2014-09-18 10:27:32 -0300

    Add submission title in workflow pages header

--- plugins/themes/scielo/ScieloThemePlugin.inc.php
+++ plugins/themes/scielo/ScieloThemePlugin.inc.php
@@ -25,19 +25,55 @@ class ScieloThemePlugin extends ThemePlugin {
 		return parent::register($category, $path);
 	}
 
+	/**
+	 * Template manager display hook callback to register smarty filters.
+	 * @param $hookName string
+	 * @param $args array
+	 * @return boolean
+	 */
 	function callbackDisplay($hookName, $args) {
 		$smarty = $args[0]; /* @var $smarty Smarty */
 		$smarty->register_outputfilter(array(&$this, 'addPluginClassOutputFilter'));
+		$smarty->register_prefilter(array(&$this, 'addArticleTitleInWorkflowPagesHeader'));
+
+		return false;
 	}
 
 	/**
-	 *
-	 * @param unknown_type $output
-	 * @param unknown_type $smarty
-	 * @return mixed
+	 * Output filter to add the plugin css class into the body html element.
+	 * @param $output string
+	 * @param $smarty Smarty
+	 * @return string
 	 */
 	function addPluginClassOutputFilter($output, &$smarty) {
 		$output = str_replace('<body id="pkp-common-openJournalSystems">', '<body id="pkp-common-openJournalSystems" class="scieloTheme">', $output);
+		return $output;
+	}
+
+	/**
+	 * Smarty prefilter to modify the template markup before it's rendered,
+	 * adding the article title in workflow pages h2 element.
+	 * @param $output string
+	 * @param $smarty Smarty
+	 * @return string
+	 */
+	function addArticleTitleInWorkflowPagesHeader($output, &$smarty) {
+                $templatesToChange = array(
+			'templates/sectionEditor/submission.tpl' => 'submission.page.summary',
+			'templates/sectionEditor/submissionReview.tpl' => 'submission.page.review',
+			'templates/sectionEditor/submissionEditing.tpl' => 'submission.page.editing',
+			'templates/sectionEditor/submissionHistory.tpl' => 'submission.page.history',
+			'templates/sectionEditor/submissionCitations.tpl' => 'submission.page.citations'
+		);
+
+		foreach ($templatesToChange as $templateFile => $localeKey) {
+			if (strpos($output, ' * ' . $templateFile) !== false) {
+				$markupToBeReplaced = '{translate|assign:"pageTitleTranslated" key="' . $localeKey . '" id=$submission->getId()}';
+				$replaceMarkup = $markupToBeReplaced . '{assign var="pageTitleTranslated" value=$pageTitleTranslated|concat:" - ":$submission->getLocalizedTitle()|strip_unsafe_html}'; 
+				$output = str_replace($markupToBeReplaced, $replaceMarkup, $output);
+			}
+		}
+
 		return $output;
 	}
 
